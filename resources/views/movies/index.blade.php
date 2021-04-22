f69b7081d60ccf22904b86b856e78ea87aad0686
nflorijan <florijan.nadj@gmail.com>
2021-04-22 17:09:06 +0200

    Create comments table add add values

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -8,7 +8,7 @@
     @foreach ($movies as $movie)
       <li class="list-group-item">
         <h4><a href="{{ route('movie', ['movie' => $movie->id]) }}">{{ $movie->title }}</a></h4>
-        <p>{{ $movie->storyline }}</p>
+        <p>{{ Str::limit($movie->storyline, 80, '...') }}</p>
       </li>
     @endforeach
   </ul>
